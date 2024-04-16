3b814b83931779b5ef9793f6f0b426bf6cb8af85
Dmitrijs Loginovs <132077317+Loginofff@users.noreply.github.com>
2024-04-16 13:45:57 +0200

    fix category logos

--- app/_components/CategorySearch.jsx
+++ app/_components/CategorySearch.jsx
@@ -27,30 +27,30 @@ function CategorySearch() {
     }
   };
 
+
+
   return (
     <div className="mb-10 items-center flex flex-col gap-4 px-5">
-      <h2 className="font-bold text-4xl tracking-wide">
-      Wähle eine  <span className="text-green-800">Kategorie</span>{" "}
-      </h2>
-    
+    <h2 className="font-bold text-4xl tracking-wide">
+      Wähle eine <span className="text-green-800">Kategorie</span>{" "}
+    </h2>
 
+    <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 ">
+      {categoryList.map((item) => (
+       <Link href={"/search/" + item.name} key={item.id}>
+       <div className="flex flex-col text-center items-center
+        p-5 hover:scale-110 transition-all ease-in-out cursor-pointer">
+         <img src={`/${item.name}.png`} alt={item.name} width={100} height={100} />
+         <label>{item.name}</label>
+       </div>
+     </Link>
      
-
-      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 ">
-        {categoryList.map((item) => (
-          <Link href={"/search/"+item.name}
-            key={item.id}
-            className="flex flex-col text-center items-center p-5 hover:scale-110 transition-all ease-in-out cursor-pointer 
-          ">
-            <Image src="/menu-1.png" alt="logo" width={100} height={100} />
-            <label>{item.name}</label>
-          </Link>
-        ))}
-      </div>
-
+      ))}
     </div>
-  );
+  </div>
+);
 }
+
 
 export default CategorySearch;
 
